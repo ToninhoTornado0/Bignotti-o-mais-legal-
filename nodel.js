0bb7ef381c52a37b087966c0206aad5fa5cf2647
GABRIEL BIGNOTTI <gabriel_bignotti@estudante.sesisenai.org.br>
2025-05-22 09:22:20 -0300

    criação do servidor basico

--- nodel.js
+++ nodel.js
@@ -11,3 +11,14 @@ function registrarLog(nomeAluno){
 }
 
 module.export = registrarLog.Log;
+
+const express = require('express');
+const registrarLog = require('./logs');
+const app = express();
+app.use(express.json());
+
+const PORT = 8000;
+//Servidor iniciando
+app.listen(PORT, () => {
+    console.log("Servidor ta rodando na porta 8000 ")
+})
